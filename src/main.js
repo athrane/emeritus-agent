284a727dd2a4fe0b21f893a4c7c68ee6747d8e88
Allan Thrane Andersen <einheriii@gmail.com>
2025-03-23 23:03:54 +0000

    Update agent and belief initialization; change agent name to Acticus and adjust initial belief values

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
-import { Agent } from './internal.js';  
-import { IntegerPercentageBelief } from './internal.js'; 
-import { IntegerPercentageBeliefUpdater } from './internal.js'; 
+import { Agent } from './internal.js';
+import { IntegerPercentageBelief } from './internal.js';
+import { IntegerPercentageBeliefUpdater } from './internal.js';
 
 // Create a new agent with some beliefs
-const oldMan = new Agent("Bob");
-const hungerBelief =  new IntegerPercentageBelief("hunger", 100);
+const oldMan = new Agent("Acticus");
+const hungerBelief = new IntegerPercentageBelief("hunger", 0);
 const fatigueBelief = new IntegerPercentageBelief("fatigue", 0);
 oldMan.addBelief(hungerBelief);
 oldMan.addBelief(fatigueBelief);
-oldMan.registerBeliefUpdater(IntegerPercentageBeliefUpdater(hungerBelief, -10));
+oldMan.registerBeliefUpdater(new IntegerPercentageBeliefUpdater(hungerBelief, 5));
 oldMan.registerBeliefUpdater(new IntegerPercentageBeliefUpdater(fatigueBelief, 5));
 
 // Simulate the NPC's behavior over time
 for (let i = 0; i < 100; i++) {
   console.log(`--- Tick ${i + 1} ---`);
-//  oldMan.update();
+  oldMan.update();
 }
 
