2aeb6ff26f75498ea14d374805a44fbad4e9ab16
Allan Thrane Andersen <einheriii@gmail.com>
2025-06-28 07:43:33 +0000

    Add new rooms to scene: Study, bathroom, upper hall, closes #78.

--- src/bdiagent/movement/SceneFactory.js
+++ src/bdiagent/movement/SceneFactory.js
@@ -32,7 +32,19 @@ export class SceneFactory {
         const livingRoom = scene.createRoom("Living Room",2,0,1,1);
         const hall = scene.createRoom("Hall",3,0,1,1);
         const garden = scene.createRoom("Garden",4,0,1,1);
+        const upperHall = scene.createRoom("Upper Hall",2.5,1,1,1);
+        const bathroom = scene.createRoom("Bathroom",1.5,1,1,1);
+        const study = scene.createRoom("Study",0.5,1,1,1);
 
+        // connect rooms
+        SceneFactory.registerAdjacentRooms(bedroom, kitchen);
+        SceneFactory.registerAdjacentRooms(kitchen, livingRoom);
+        SceneFactory.registerAdjacentRooms(livingRoom, hall);
+        SceneFactory.registerAdjacentRooms(hall, garden);
+        SceneFactory.registerAdjacentRooms(hall, upperHall);
+        SceneFactory.registerAdjacentRooms(upperHall, bathroom);
+        SceneFactory.registerAdjacentRooms(bathroom, study);
+        
         // create and register locations
         bedroom.createLocation("Bed", 0.5, 0.2); 
         bedroom.createLocation("Wardrobe", 0.25, 0.2);        
@@ -44,12 +56,13 @@ export class SceneFactory {
         hall.createLocation("Front Door", 1, 0.2);
         garden.createLocation("Flower Bed", 0.5, 0.2);
         garden.createLocation("Tree", 1, 0.2);
+        bathroom.createLocation("Sink", 0.5, 0.2);
+        bathroom.createLocation("Toilet", 0.25, 0.2);
+        bathroom.createLocation("Shower", 0.75, 0.2);
+        study.createLocation("Desk", 0.5, 0.2);
+        study.createLocation("Chair", 0.25, 0.2);
+        study.createLocation("Bookshelf", 0.75, 0.2);
 
-        // connect rooms
-        SceneFactory.registerAdjacentRooms(bedroom, kitchen);
-        SceneFactory.registerAdjacentRooms(kitchen, livingRoom);
-        SceneFactory.registerAdjacentRooms(livingRoom, hall);
-        SceneFactory.registerAdjacentRooms(hall, garden);
         return scene;
     }        
 
