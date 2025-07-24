b8a619f7b886ec926410acc65c1fbce46628d1d4
Allan Thrane Andersen <einheriii@gmail.com>
2025-07-24 13:41:47 +0000

    Refactor SceneFactory to new directory and update internal exports; add coverage directory to .gitignore #113

--- src/internal.js
+++ src/internal.js
@@ -11,7 +11,7 @@ export { Component } from './component/Component.js';
 export { Position } from './bdiagent/movement/Position.js';
 export { Location } from './bdiagent/movement/Location.js';
 export { Room } from './bdiagent/movement/Room.js';
-export { SceneFactory } from './bdiagent/movement/SceneFactory.js';
+export { SceneFactory } from './scene/SceneFactory.js';
 export { Scene } from './scene/Scene.js';
 export { Path } from './bdiagent/movement/Path.js';
 export { Motion } from './bdiagent/movement/Motion.js';
