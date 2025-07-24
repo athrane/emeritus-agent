181d7b896c1bf58590ea554c7aabe1cc8984883a
Allan Thrane Andersen <einheriii@gmail.com>
2025-07-24 13:35:20 +0000

    Refactor Scene class and move to new directory; update internal exports accordingly, closes #112

--- src/bdiagent/movement/Scene.js
+++
@@ -1,5 +0,0 @@
-import { TypeUtils } from '../../internal.js';
-import { Position } from '../../internal.js';
-import { Room } from '../../internal.js';
-import { Location } from '../../internal.js';
-import { Path } from '../../internal.js';
